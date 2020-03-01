170a527019fcaccbff5099a00ac0c38fac00687b
Ben Denney <denneyb@science.regn.net>
2020-03-01 21:05:24 +0000

    changed for kmowledge share

--- Dockertutorials/express/server.js
+++ Dockertutorials/express/server.js
@@ -6,7 +6,7 @@ const PORT = 80;
     //use the app variable to make a get and point, with two parameters: a string where we expose end point, which is the root of the project here; and a callback function in arrow function format of javascript.  Two parameters are within the parenthesis- request, and a response (send back a "Hello Worrld!" string).
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! Put your message HERE!');
 });
 
 app.listen(PORT, HOST);  //make the app live by getting the app to listen on the PORT defined above, and to listen on the HOST defined above.
