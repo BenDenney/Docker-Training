cd4ec2fcab1c4dd963b70eb830769f98f4a3dbae
Ben Denney <denneyb@science.regn.net>
2020-03-01 22:15:57 +0000

    changed server.js file for knowledge share

--- Dockertutorials/express2/app/server.js
+++ Dockertutorials/express2/app/server.js
@@ -6,8 +6,28 @@ const PORT = 80;
     //use the app variable to make a get and point, with two parameters: a string where we expose end point, which is the root of the project here; and a callback function in arrow function format of javascript.  Two parameters are within the parenthesis- request, and a response (send back a "Hello Worrld!" string).
 
 app.get('/', (req, res) => {
-    res.send('Hello World! 1: I have changed after the first docker build, without changing the package.json file, to see if docker build will use the cached package.json file and npm install, and thus only redo the app folder copy and default command');
+    res.send('Hello World! 1: Enter your first text here');
 });
 
 app.listen(PORT, HOST);  //make the app live by getting the app to listen on the PORT defined above, and to listen on the HOST defined above.
 console.log(`Running on http://${HOST}:${PORT}`);  //give a message to our user.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////  second comment- I have changed after the first docker build, without changing the package.json file, to see if docker build will use the cached package.json file and npm install, and thus only redo the app folder copy and default command
